f8bd63801a560f84e3d2b213ccba0f85ed90ded6
AmirHJ <amirhj1378@gmail.com>
2020-12-21 01:50:54 +0330

    theme
    theme basis added

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './shared/Theming/theme'
 
 ReactDOM.render(
-  <Router><App /></Router>
+  <ThemeProvider theme={theme}>
+    <Router><App /></Router>
+  </ThemeProvider>
+
   ,
   document.getElementById('root')
 );
---
+++ src/shared/Theming/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import { blue, red } from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main:blue[500]
+        },
+        secondary: {
+            main:red[500]
+        }
+    }
+
+})
+export default theme
