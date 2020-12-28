4d67effa582c35c93838417e18b87ab265f3f1bd
AmirHJ <amirhj1378@gmail.com>
2020-12-29 02:58:28 +0330

    login Griding nearly okeyed
    important things was happened..!!!!!!!!!!!!!!111

--- src/user/pages/Login.js
+++ src/user/pages/Login.js
@@ -42,6 +42,7 @@ const useStyles = makeStyles((theme) => ({
     margin: 'auto',
   },
   field: {
+    margin:theme.spacing(4)
   }
 }));
 
@@ -50,7 +51,7 @@ export default function SignInSide() {
 
   return (
     <Grid container className={classes.root}>
-      <Grid container item sm={7} >       {/* its children is column....this is row!!!!*/}
+      <Grid container item sm={7}  >       {/* its children is column....this is row!!!!*/}
         <Grid item sm={12}>
           <img
             src={khayam}
@@ -60,33 +61,41 @@ export default function SignInSide() {
         </Grid>
       </Grid>
 
-      <Grid container item sm={5} direction="row" justify="center" >    {/* its children is column....this is row!!!!*/}
+      <Grid container item sm={5} justify="center" >    {/* its children is column....this is row!!!!*/}
+        <Grid item container sm direction="column">
 
-        <Grid item sm={12} style={{ textAlign: 'center' }}>
-          <img src={logo} className={classes.logo} alt="logo" />
-        </Grid>
+          <Grid item sm={4}>
+            <Grid container item sm={12} justify="center"  >
+              <Grid item sm={12} style={{ textAlign: 'center' }}>
+                <img src={logo} className={classes.logo} alt="logo" />
+              </Grid>
+              <Grid item sm={12} style={{ textAlign: 'center' }} >
+                <Typography variant={'h6'}>
+                  سیستم آزمایشی انتخاب واحد
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
 
-        <Grid item sm={12} style={{ textAlign: 'center' }} >
-          <Typography variant={'h6'}>
-            سیستم آزمایشی انتخاب واحد
-          </Typography>
-        </Grid>
-        <Grid container item sm={12} justify="center" spacing={4}>
-          <form noValidate autoComplete="off">
-            <Grid item sm={12}>
-              <TextField id="standard-basic" label="Standard" />
+          <Grid item sm={8}>
+            <Grid container item sm={12} justify="center">
+              <form noValidate autoComplete="off">
+                <Grid item sm={12}>
+                  <TextField id="standard-basic" label="Standard" className={classes.field}/>
+                </Grid>
+                <Grid item sm={12}>
+                  <TextField id="standard-basic" label="Standard" className={classes.field} />
+                </Grid>
+                <Grid item sm={12}>
+                  <TextField id="standard-basic" label="Standard" className={classes.field} />
+                </Grid>
+              </form>
             </Grid>
-            <Grid item sm={12}>
-              <TextField id="standard-basic" label="Standard" />
-            </Grid>
-            <Grid item sm={12}>
-              <TextField id="standard-basic" label="Standard" />
-            </Grid>
-          </form>
-        </Grid>
+          </Grid>
 
+
+        </Grid>
       </Grid>
-
     </Grid>
   );
 }
